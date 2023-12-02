a756c9f74f1a99e4ef6a9a04e92105828a0cfe72
hwld <0701hw@gmail.com>
2023-12-02 17:46:54 +0900

    refactor: cvaを使わないで書き直した

--- frontend/src/app/_components/button.tsx
+++ frontend/src/app/_components/button.tsx
@@ -1,38 +1,15 @@
 import { useDebouncedValue } from '@mantine/hooks';
-import { VariantProps, cva } from 'cva';
+import { cx } from 'cva';
 import { Loader2Icon, LucideIcon } from 'lucide-react';
 import { ComponentPropsWithoutRef, ReactNode } from 'react';
-
-const button = cva({
-  base: 'rounded transition-all focus-visible:outline-none focus-visible:ring-2 ring-offset-2 flex items-center gap-1 disabled:opacity-50 disabled:pointer-events-none ring-offset-neutral-200 ring-neutral-500 disabled:cursor-not-allowed',
-  variants: {
-    color: {
-      black: 'bg-neutral-800 text-neutral-100 hover:bg-neutral-600',
-      white:
-        'bg-neutral-100 text-neutral-700 hover:bg-neutral-200 border border-neutral-300',
-    },
-    size: {
-      md: 'py-2 px-4 text-base',
-      sm: 'py-1 px-3 text-sm',
-    },
-  },
-  defaultVariants: {
-    color: 'black',
-    size: 'md',
-  },
-});
-
-const iconSize = {
-  md: '20',
-  sm: '18',
-};
 
 type Props = {
   children: ReactNode;
   leftIcon?: LucideIcon;
   debouncedIsLoading?: boolean;
-} & Omit<ComponentPropsWithoutRef<'button'>, 'className'> &
-  VariantProps<typeof button>;
+  size?: 'md' | 'sm';
+  color?: 'black' | 'white';
+} & Omit<ComponentPropsWithoutRef<'button'>, 'className'>;
 
 export const Button: React.FC<Props> = ({
   children,
@@ -45,15 +22,31 @@ export const Button: React.FC<Props> = ({
 }) => {
   const [debouncedIsLoading] = useDebouncedValue(isLoading, 500);
 
+  const iconSize = {
+    md: '20',
+    sm: '18',
+  };
+
   return (
     <button
       disabled={isLoading || disabled}
       {...props}
-      className={button({ color, size })}
+      className={cx(
+        'flex items-center gap-1 rounded ring-neutral-500 ring-offset-2 ring-offset-neutral-200 transition-all focus-visible:outline-none focus-visible:ring-2 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50',
+        {
+          white:
+            'border border-neutral-300 bg-neutral-100 text-neutral-700 hover:bg-neutral-200',
+          black: 'bg-neutral-800 text-neutral-100 hover:bg-neutral-600',
+        }[color],
+        {
+          md: 'px-4 py-2 text-base',
+          sm: 'px-3 py-1 text-sm',
+        }[size],
+      )}
     >
       {debouncedIsLoading && (
         <Loader2Icon
-          size={iconSize[size]}
+          size={cx(iconSize[size])}
           className="animate-spin"
           strokeWidth={3}
         />
--- frontend/src/app/_components/icon-button.tsx
+++ frontend/src/app/_components/icon-button.tsx
@@ -1,18 +1,14 @@
-import { VariantProps, cva } from 'cva';
+import { cx } from 'cva';
 import { LucideIcon } from 'lucide-react';
 import { ComponentPropsWithoutRef, forwardRef } from 'react';
 
-const iconButton = cva({
-  base: 'flex items-center justify-center bg-transparent hover:bg-neutral-200 transition-all rounded disabled:pointer-events-none disabled:opacity-50 focus-visible:outline-none focus-visible:ring-2 ring-offset-2 ring-neutral-500 text-neutral-700',
-  variants: { size: { md: 'p-1 w-[30px] h-[30px]' } },
-  defaultVariants: { size: 'md' },
-});
-
-type Props = { icon: LucideIcon } & VariantProps<typeof iconButton> &
-  ComponentPropsWithoutRef<'button'>;
+type Props = {
+  icon: LucideIcon;
+  size?: 'md';
+} & ComponentPropsWithoutRef<'button'>;
 
 export const IconButton: React.FC<Props> = forwardRef<HTMLButtonElement, Props>(
-  function IconButton({ icon: Icon, size, onClick, ...props }, ref) {
+  function IconButton({ icon: Icon, size = 'md', onClick, ...props }, ref) {
     const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
       e.stopPropagation();
       onClick?.(e);
@@ -23,7 +19,10 @@ export const IconButton: React.FC<Props> = forwardRef<HTMLButtonElement, Props>(
         ref={ref}
         onClick={handleClick}
         {...props}
-        className={iconButton({ size })}
+        className={cx(
+          'flex items-center justify-center rounded bg-transparent text-neutral-700 ring-neutral-500 ring-offset-2 transition-all hover:bg-neutral-200 focus-visible:outline-none focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50',
+          { md: 'h-[30px] w-[30px] p-1' }[size],
+        )}
       >
         <Icon size={20} />
       </button>
