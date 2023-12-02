c927065773a7c5ecaf8a70b7ed581abfe1a463ed
hwld <0701hw@gmail.com>
2023-12-03 07:55:07 +0900

    chore: urqlをアンインストールする

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,14 +1,9 @@
 'use client';
 
 import { ApolloProvider } from '@/lib/apollo';
-import { UrqlProvider } from '@/lib/urql';
 
 export const Providers: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  return (
-    <ApolloProvider>
-      <UrqlProvider>{children}</UrqlProvider>
-    </ApolloProvider>
-  );
+  return <ApolloProvider>{children}</ApolloProvider>;
 };
