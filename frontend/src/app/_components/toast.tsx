937b323bbfb322fa59f0053b081cfd9f59d96dd9
hwld <0701hw@gmail.com>
2023-12-05 20:38:17 +0900

    refactor: toastのアニメーションをframer-motionで実装する

--- frontend/src/app/_components/toast.tsx
+++ frontend/src/app/_components/toast.tsx
@@ -10,7 +10,7 @@ import {
 import * as RadixToast from "@radix-ui/react-toast";
 import { XIcon } from "lucide-react";
 import { cx } from "cva";
-import { LayoutGroup, motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 type ToastItem = {
   id: string;
@@ -49,48 +49,46 @@ const Toast: React.FC<ToastProps> = ({ toast, onClose }) => {
       onOpenChange={handleOpenChange}
       duration={toast.duration}
       asChild
+      forceMount
     >
       <motion.div
         layout
+        drag="x"
+        dragConstraints={{ left: 0, right: 100 }}
+        dragElastic={{ left: 0, right: 0.5 }}
+        dragSnapToOrigin
         className={cx(
-          "group",
-          "cursor-pointer",
-          "data-[state=closed]:animate-toastSlideRight",
+          "relative my-1 flex h-fit cursor-pointer items-center justify-between gap-1 rounded-lg bg-neutral-900 p-3 text-neutral-100 shadow-md",
         )}
-        initial={{ opacity: 0, x: 30 }}
+        initial={{ opacity: 0, x: 50 }}
         animate={{ opacity: 1, x: 0 }}
+        exit={{
+          opacity: 0,
+          transform: "translateX(100%)",
+          transition: { duration: 0.15 },
+        }}
       >
-        <div
-          className={cx(
-            "relative my-1 flex h-fit items-center justify-between gap-1 rounded-lg bg-neutral-900 p-3 text-neutral-100 shadow-md",
-            // motion.divに渡しても動かないことがあるので、もう一つdivを追加した
-            "group-data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)]",
-            "group-data-[swipe=cancel]:translate-x-0",
-            "group-data-[swipe=cancel]:transition-[transform]",
-          )}
-        >
-          <div className="flex h-full gap-1">
-            <div
-              className={cx(
-                "absolute bottom-2 left-2 top-2 w-[4px] rounded-full",
-                toast.type === "success" && "bg-green-600",
-                toast.type === "error" && "bg-red-500",
-              )}
-            />
-            <div className="ml-2 flex flex-col gap-1">
-              <RadixToast.Title className="text-sm">
-                {toast.title}
-              </RadixToast.Title>
-              <RadixToast.Description className="text-xs text-neutral-300">
-                {toast.description}
-              </RadixToast.Description>
-            </div>
+        <div className="flex h-full gap-1">
+          <div
+            className={cx(
+              "absolute bottom-2 left-2 top-2 w-[4px] rounded-full",
+              toast.type === "success" && "bg-green-600",
+              toast.type === "error" && "bg-red-500",
+            )}
+          />
+          <div className="ml-2 flex flex-col gap-1">
+            <RadixToast.Title className="text-sm">
+              {toast.title}
+            </RadixToast.Title>
+            <RadixToast.Description className="text-xs text-neutral-300">
+              {toast.description}
+            </RadixToast.Description>
           </div>
-          <div>
-            <RadixToast.Close className="flex items-center rounded transition-colors hover:bg-neutral-700">
-              <XIcon size={20} />
-            </RadixToast.Close>
-          </div>
+        </div>
+        <div>
+          <RadixToast.Close className="flex items-center rounded transition-colors hover:bg-neutral-700">
+            <XIcon size={20} />
+          </RadixToast.Close>
         </div>
       </motion.div>
     </RadixToast.Root>
@@ -114,13 +112,17 @@ export const ToastProvider: React.FC<ToastProviderProps> = ({ children }) => {
 
   return (
     <ToastContext.Provider value={{ toast: openToast }}>
-      <RadixToast.Provider duration={5000} swipeThreshold={100}>
+      <RadixToast.Provider duration={500000} swipeThreshold={100}>
         {children}
-        <LayoutGroup>
-          {toasts.map((toast) => (
-            <Toast key={toast.id} toast={toast} onClose={closeToast} />
-          ))}
-        </LayoutGroup>
+        <AnimatePresence>
+          {toasts.map((toast) => {
+            return (
+              toast.isOpen && (
+                <Toast key={toast.id} toast={toast} onClose={closeToast} />
+              )
+            );
+          })}
+        </AnimatePresence>
         <RadixToast.Viewport className="fixed bottom-[70px] right-[10px] z-[100] w-[350px]" />
       </RadixToast.Provider>
     </ToastContext.Provider>
