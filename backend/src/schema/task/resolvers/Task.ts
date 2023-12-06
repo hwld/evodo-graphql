34399de55756c6518d5a48c4abdf723edcf04a86
hwld <0701hw@gmail.com>
2023-12-06 23:25:51 +0900

    feat: Taskにauthorを追加

--- backend/src/schema/task/resolvers/Task.ts
+++ backend/src/schema/task/resolvers/Task.ts
@@ -1,4 +1,16 @@
+import { GraphQLError } from "graphql";
 import type { TaskResolvers } from "./../../types.generated";
 export const Task: TaskResolvers = {
   /* Implement Task resolver logic here */
+  author: async (task, _, { db }) => {
+    const user = await db.task
+      .findUnique({ where: { id: task.id.toString() } })
+      .user();
+
+    if (!user) {
+      throw new GraphQLError("not found");
+    }
+
+    return user;
+  },
 };
